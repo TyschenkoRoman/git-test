d48039351671ce5f75aebcb19a4120c90aa423af
Roman <tyschenkoroman@gmail.com>
2016-07-14 22:19:25 +0300

    Just the first commint

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""print ("Hello man! I am Python.")
+name = input ("Who are you? ")
+print ("Fuck you, " + name + "!")
+"""
+
+from datetime import datetime
+now = datetime.now()
+print(now.day, "/", now.month, "/", now.year, sep="")
